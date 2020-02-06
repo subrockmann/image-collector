12bc8a116742fb3a061a5db70f4dd0d1319ae141
djlisashi <newinmunich@aol.com>
2020-02-06 20:57:28 +0100

    feat: add uploads form

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,17 @@
-from flask import render_template
-from app import app
+from flask import render_template 
+from flask import request, redirect
+from flask_wtf import FlaskForm
 
-@app.route('/index')   # this does not work
+
+from werkzeug.utils import secure_filename
+from app import app
+from pathlib import Path
+import os
+
+uploads_folder = Path('/uploads')
+
+@app.route('/')
+@app.route('/index')   
 def index():
     #return "Index"
     user = {'username': 'Peter'}
@@ -10,6 +20,27 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/upload')
+def upload():
+    return render_template('upload.html')
+
+#############
+@app.route("/upload-image", methods=["GET", "POST"])
+def upload_image():
+
+    if request.method == "POST":
+
+        if request.files:
+
+            image = request.files["image"]
+
+            print(image)
+
+            return redirect(request.url)
+
+
+    return render_template("success_upload.html") 
 
 
 @app.route('/hello')
