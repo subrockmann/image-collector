030d668601895f8e6d9cafa06ab8d3d5df788473
djlisashi <newinmunich@aol.com>
2020-02-07 16:43:21 +0100

    feat: add file upload

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template 
-from flask import request, redirect
+from flask import request, redirect, flash
 from flask_wtf import FlaskForm
 
 
@@ -7,8 +7,13 @@ from werkzeug.utils import secure_filename
 from app import app
 from pathlib import Path
 import os
+from os.path import dirname
 
-uploads_folder = Path('/uploads')
+UPLOAD_FOLDER = os.path.join(dirname(dirname(os.path.realpath(__file__))), 'uploads')
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 
 @app.route('/')
 @app.route('/index')   
@@ -25,17 +30,19 @@ def about():
 def upload():
     return render_template('upload.html')
 
-#############
+
 @app.route("/upload-image", methods=["GET", "POST"])
 def upload_image():
 
     if request.method == "POST":
 
         if request.files:
-
             image = request.files["image"]
+            filename = secure_filename(image.filename)
+            #flash("Image saved.")   # using flash requires setting o a secret key
 
-            print(image)
+            # TODO: replace this part with a function to upload to s3
+            image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
             return redirect(request.url)
 
