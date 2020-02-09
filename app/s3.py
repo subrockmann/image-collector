51885e7045ed546feaf89ff42aa23238c7b4579f
djlisashi <newinmunich@aol.com>
2020-02-09 21:40:21 +0100

    feat: create a bucket if it does not exist

--- app/s3.py
+++ app/s3.py
@@ -1,5 +1,8 @@
 import boto3
 import botocore
+from botocore.exceptions import ClientError
+
+import logging
 import pandas as pd
 import os
 from os.path import dirname
@@ -17,6 +20,8 @@ AWS_SECRET = pd.read_csv(CREDENTIALS_FILE)['Secret access key'][0]
 MY_REGION = 'eu-central-1'
 BUCKET_NAME = 'data-collector-v1'
 
+s3_resource = boto3.resource('s3')
+
 def setup_s3():
     # Generate the boto3 client for interacting with S3 and SNS
     s3 = boto3.client('s3',  region_name= MY_REGION,   
@@ -24,3 +29,25 @@ def setup_s3():
         aws_secret_access_key=AWS_SECRET)
     return s3
 
+def create_bucket_if_not_exists(bucket_name):    
+    exists = True
+    try:
+        s3.head_bucket(Bucket=bucket_name)
+        
+    except botocore.exceptions.ClientError as e:
+        # If a client error is thrown, then check that it was a 404 error.
+        # If it was a 404 error, then the bucket does not exist.
+        error_code = e.response['Error']['Code']
+        if error_code == '404':
+            exists = False
+        s3.create_bucket(Bucket=bucket_name,
+                             CreateBucketConfiguration={'LocationConstraint': MY_REGION})
+    return s3_resource.Bucket(name=bucket_name)
+
+
+
+
+
+# connect to S3
+s3 = setup_s3()
+create_bucket_if_not_exists(BUCKET_NAME)
--- application.py
+++ application.py
@@ -1,8 +1,13 @@
 from app import app
+from app import s3
 
 @app.route('/')
 def home():
     return "Hello World - Index"
+
+
+
+
 
 if __name__ == "__main__":
     app.debug = True
